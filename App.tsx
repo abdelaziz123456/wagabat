27ed8c02ff63edf04a188c0c0ea4ea6a178d98db
abdelaziz123456 <abdelazizmohamed1233@gmail.com>
2023-05-08 03:29:52 +0300

    finish onboarding screen

--- App.tsx
+++ App.tsx
@@ -3,65 +3,93 @@ import * as Animatable from 'react-native-animatable';
 
 import {Image, Text, View} from 'react-native';
 import CustomButton from './src/SharedComponents/CustomButton/CustomButton';
+import CustomColors from './src/Utiles/constants';
 
 function App() {
   const [currScreen, setCurrentScreen] = useState(1);
   const animatableRef = useRef(null);
+  const textRef = useRef(null);
+  const contentRef = useRef(null);
 
   const bounceLeft = () => {
     animatableRef.current?.fadeInLeft(500);
+    textRef.current?.fadeInLeft(500);
+    contentRef.current?.fadeInLeft(500);
   };
 
   const bounceRight = () => {
     animatableRef.current?.fadeInRight(500);
+    textRef.current?.fadeInRight(500);
+    contentRef.current?.fadeInRight(500);
   };
   const screensData = {
     1: {
       img: require('./assets/images/food1.jpg'),
-      title: 'Wide range of FoodCategories & more',
+      title: 'Wide range of Food \n Categories & more',
       content:
-        "Browse through our extensive list of restaurants and dishes, and when you're readyto order, simply add your desired items to your cart and checkout. It's that easy!",
+        "Browse through our extensive list of \n restaurants and dishes, and when you're  ready \n to order, simply add your desired items to your \n cart and checkout. It's that easy!",
     },
     2: {
       img: require('./assets/images/food2.jpg'),
-      title: 'Free Deliveries for ONE MONTH!!',
+      title: 'Free Deliveries for \n ONE MONTH!!',
       content:
         'Get your favorite meals delivered to your doorstep for free with our online food deliveryapp - enjoy a whole month of complimentary delivery!',
     },
     3: {
       img: require('./assets/images/food3.jpg'),
-      title: 'Get started on Ordering Your Food',
+      title: 'Get started on \n Ordering Your Food',
       content:
         'Please create an account or sign in to your existing account to start browsing our selection of delicious meals from your favoriterestaurants.',
     },
   };
 
   return (
-    <View style={{backgroundColor: 'white', flex: 1, alignItems: 'center'}}>
-      <Text>Hello</Text>
+    <View
+      style={{
+        backgroundColor: 'white',
+        flex: 1,
+        alignItems: 'center',
+        paddingHorizontal: 21,
+      }}>
+      <Animatable.View ref={animatableRef} style={{alignItems: 'center'}}>
+        <Image
+          source={screensData[currScreen].img}
+          style={{
+            height: 350,
+            resizeMode: 'stretch',
+            width: 350,
+            borderRadius: 20,
+            marginTop: 28,
+          }}
+        />
+        <Text style={{marginBottom: 50, marginTop: 10}}>{currScreen} of 3</Text>
+        <Text
+          style={{
+            fontSize: 32,
+            fontWeight: '600',
+            textAlign: 'left',
+            color: CustomColors.dark100,
+            marginBottom: 13,
+          }}>
+          {screensData[currScreen].title}
+        </Text>
+        <Text style={{lineHeight: 20, marginBottom: 30, minHeight: 80}}>
+          {screensData[currScreen].content}
+        </Text>
+      </Animatable.View>
 
-      <Animatable.Image
-        source={screensData[currScreen].img}
-        ref={animatableRef}
-        style={{
-          height: 350,
-          resizeMode: 'stretch',
-          width: 350,
-          borderRadius: 20,
-        }}
-      />
       <View style={{flexDirection: 'row', justifyContent: 'center'}}>
         <CustomButton
           pressHandler={() => {
-            if (currScreen > 1) {
-              setCurrentScreen(currScreen - 1);
-              bounceLeft();
+            if (currScreen !== 3) {
+              setCurrentScreen(3);
+              bounceRight();
             }
           }}
           //CustomStyles={{backgroundColor: 'red'}}
           btnTheme={'white'}>
           <View style={{flexDirection: 'row'}}>
-            <Text style={{fontSize: 18}}>hello</Text>
+            <Text style={{fontSize: 18}}>Skip</Text>
             <Image
               source={require('./assets/icons/arrowRightBlack.png')}
               alt=""
@@ -78,7 +106,9 @@ function App() {
           //CustomStyles={{backgroundColor: 'red'}}
           btnTheme={'blue'}>
           <View style={{flexDirection: 'row'}}>
-            <Text style={{color: 'white', fontSize: 18}}>hello</Text>
+            <Text style={{color: 'white', fontSize: 18}}>
+              {currScreen == 3 ? 'Continue' : 'Next'}
+            </Text>
             <Image source={require('./assets/icons/arrow-right.png')} alt="" />
           </View>
         </CustomButton>
---
+++ src/screens/WelcomeScreen/WelcomeScreen.tsx
@@ -0,0 +1,91 @@
+import {View, Text, Image} from 'react-native';
+import React, {useState, useRef} from 'react';
+import * as Animatable from 'react-native-animatable';
+import CustomButton from '../../SharedComponents/CustomButton/CustomButton';
+export default function WelcomeScreen() {
+  const [currScreen, setCurrentScreen] = useState(1);
+  const animatableRef = useRef(null);
+  const textRef = useRef(null);
+
+  const bounceLeft = () => {
+    animatableRef.current?.fadeInLeft(500);
+    textRef.current?.fadeInLeft(500);
+  };
+
+  const bounceRight = () => {
+    animatableRef.current?.fadeInRight(500);
+    textRef.current?.fadeInRight(500);
+  };
+  const screensData = {
+    1: {
+      img: require('./assets/images/food1.jpg'),
+      title: 'Wide range of FoodCategories & more',
+      content:
+        "Browse through our extensive list of restaurants and dishes, and when you're readyto order, simply add your desired items to your cart and checkout. It's that easy!",
+    },
+    2: {
+      img: require('./assets/images/food2.jpg'),
+      title: 'Free Deliveries for ONE MONTH!!',
+      content:
+        'Get your favorite meals delivered to your doorstep for free with our online food deliveryapp - enjoy a whole month of complimentary delivery!',
+    },
+    3: {
+      img: require('./assets/images/food3.jpg'),
+      title: 'Get started on Ordering Your Food',
+      content:
+        'Please create an account or sign in to your existing account to start browsing our selection of delicious meals from your favoriterestaurants.',
+    },
+  };
+  return (
+    <View style={{backgroundColor: 'white', flex: 1, alignItems: 'center'}}>
+      <Animatable.Image
+        source={screensData[currScreen].img}
+        ref={animatableRef}
+        style={{
+          height: 350,
+          resizeMode: 'stretch',
+          width: 350,
+          borderRadius: 20,
+          marginTop: 28,
+          marginBottom: 70,
+        }}
+      />
+
+      <Animatable.Text ref={textRef}>Hello</Animatable.Text>
+
+      <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        <CustomButton
+          pressHandler={() => {
+            if (currScreen > 1) {
+              setCurrentScreen(currScreen - 1);
+              bounceLeft();
+            }
+          }}
+          //CustomStyles={{backgroundColor: 'red'}}
+          btnTheme={'white'}>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{fontSize: 18}}>hello</Text>
+            <Image
+              source={require('./assets/icons/arrowRightBlack.png')}
+              alt=""
+            />
+          </View>
+        </CustomButton>
+        <CustomButton
+          pressHandler={() => {
+            if (currScreen < 3) {
+              setCurrentScreen(currScreen + 1);
+              bounceRight();
+            }
+          }}
+          //CustomStyles={{backgroundColor: 'red'}}
+          btnTheme={'blue'}>
+          <View style={{flexDirection: 'row'}}>
+            <Text style={{color: 'white', fontSize: 18}}>hello</Text>
+            <Image source={require('./assets/icons/arrow-right.png')} alt="" />
+          </View>
+        </CustomButton>
+      </View>
+    </View>
+  );
+}
