6e0bdf4d75c1f2c655ed35d9b5aa0f0863ee090b
abdelaziz123456 <abdelazizmohamed1233@gmail.com>
2023-06-19 10:43:21 +0300

    update size card

--- src/screens/ProductDetails/molecules/SizeCard/SizeCard.tsx
+++ src/screens/ProductDetails/molecules/SizeCard/SizeCard.tsx
@@ -2,23 +2,31 @@ import {Text, View} from 'react-native';
 import React, {useMemo, useState} from 'react';
 import {CardBase} from '@SharedComponents/index';
 import styles from './SizeCard.styles';
-import RadioGroup, {
-  RadioButton,
-  RadioButtonProps,
-} from 'react-native-radio-buttons-group';
+import RadioGroup, {RadioButtonProps} from 'react-native-radio-buttons-group';
+import {CustomColors} from '@Utiles/constants';
 
 export default function SizeCard() {
   const radioButtons: RadioButtonProps[] = useMemo(
     () => [
       {
         id: '1', // acts as primary key, should be unique and non-empty string
-        label: 'Option 1',
+        label: 'Small',
         value: 'option1',
+        borderColor: CustomColors.blue100,
+        color: CustomColors.blue100,
+        labelStyle: {
+          color: 'black',
+        },
       },
       {
         id: '2',
-        label: 'Option 2',
+        label: 'Large',
         value: 'option2',
+        borderColor: CustomColors.blue100,
+        color: CustomColors.blue100,
+        labelStyle: {
+          color: 'black',
+        },
       },
     ],
     [],
